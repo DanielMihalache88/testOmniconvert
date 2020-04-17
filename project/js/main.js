8932eb72dbbf9b1b08edb7a6a9c6823af0bba507
Daniel Mihalache <danielmihalache99@yahoo.com>
2020-04-16 23:19:06 -0700

    update on slider

--- project/js/main.js
+++ project/js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
     tabs();
     setInterval(getNews, 180000);
 });
@@ -24,67 +24,72 @@ function getNews() {
         method: 'GET',
         url: 'http://www.mocky.io/v2/58fda6ce0f0000c40908b8c8',
         dataType: 'jsonp'
-    }).done(function(data){
+    }).done(function(data) {
         $('.news__content').html('');
         var news = data.news;
 
         //display news to UI
         news.forEach(function(newsItem) {
-            var markup = '<div class="news__item"><h2>'+ newsItem.title +'</h2><p>' + newsItem.details + '</p></div>';
+            var markup = '<div class="news__item"><h2>' + newsItem.title + '</h2><p>' + newsItem.details + '</p></div>';
             $('.news__content').append(markup);
         });
 
         //add wrapper for every 5 news items in order to build slider
         var newsAll = $('.news__content > .news__item');
         var sliderItems;
-        for(var i = 0; i < newsAll.length; i += 5) {
+
+        for (var i = 0; i < newsAll.length; i += 5) {
             newsAll.slice(i, i + 5).wrapAll('<div class="slider-item"></div>');
             sliderItems = $('.slider-item');
-        } 
-        for(var i = 0; i < sliderItems.length; i++) {
+        }
+
+        for (var i = 0; i < sliderItems.length; i++) {
             sliderItems[i].classList.add('slide-' + [i]);
         }
-        
+
         var $dots = $('.slider-controls span');
+        var slides = $('.slider-item');
+        var current = 0;
+
         $dots.on('click', function() {
             var $dotId = $(this).attr('data-slide');
             $(this).siblings().removeClass('js--active');
             $(this).addClass('js--active');
             $('.slider-item').removeClass('js--show');
             $('.' + $dotId).addClass('js--show');
+            slideRight()
         });
 
-        //set first tab active
-        $('.slider-controls span:first-child').addClass('js--active');
-        $('.news__content .slider-item:first-child').addClass('js--show');
+        function slideRight() {
 
-        // functionalitatea pt derularea slideurilor automat(comentati linia 166 din main.css)
-        // var slides = $('.slider-item');
-        // var dots = $('.slider-controls span');
-        // var current = 0;
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = 'none';
+            }
 
-        // function reset() {
-        //     for( i = 0; i < slides.length; i ++) {
-        //         slides[i].style.display= 'none';
-        //     }
-        // }
+            current++
 
-        // function startSlide() {
-        //     reset();
-        //     slides[0].style.display = 'block';
-        // }
+            if (current > slides.length) {
+                current = 1;
+            }
 
-        // function slideRight() {
-        //     reset();
-        //     if(current === slides.length - 1) {
-        //         current = -1;
-        //     }
-        //     slides[current + 1].style.display = 'block';
-        //     current++;
-        // }
+            for (i = 0; i < $dots.length; i++) {
+                $dots[i].className = $dots[i].className.replace("js--active", "");
+            }
 
-        // setInterval(slideRight, 15000);
-        
+            slides[current - 1].style.display = "block";
+            $dots[current - 1].className += "js--active";
+        }
+
+        setInterval(slideRight, 3000);
+
+        $dots.on('click', function() {
+            var $dotId = $(this).attr('data-slide');
+            $(this).siblings().removeClass('js--active');
+            $(this).addClass('js--active');
+            $('.slider-item').removeClass('js--show');
+            $('.' + $dotId).addClass('js--show');
+            slideRight()
+        });
     });
 }
 getNews();
